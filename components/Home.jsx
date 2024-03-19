f47780c883878604e4a278f09ad9448ae4bc0baa
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-03-19 20:40:04 +0900

    home page layout

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,11 +1,21 @@
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import twinkle from '../assets/renderTwinkle.png';
+import {useContext} from 'react';
+import {UserLocContext} from '../contexts/userloc';
 
 export default function Home() {
+  const {userName} = useContext(UserLocContext);
   return (
-    
-        <>
-      home
-      </>
-    
+    <>
+      <Image source={twinkle} className="w-[264px] h-[291px] absolute top-[180px] left-[54px]" />
+      <View className="flex items-center justify-center top-[457px] ">
+        <Text className="text-[32px] font-bold color-[#182230]">
+          {userName}님
+        </Text>
+        <Text className="color-[#667085] text-[20px] font-medium mt-[24px]">
+          아직은 아무도 부르지 않았네요!
+        </Text>
+      </View>
+    </>
   );
 }
--- components/SplashPage.jsx
+++ components/SplashPage.jsx
@@ -9,18 +9,23 @@ export default function () {
 
   return (
     <>
-    <Header />
-    <Image className="w-full h-full absolute"  source={bg}/>
-    <Pressable onPressIn={()=>{moveLoc('home')}} className="flex flex-1 justify-center items-center">
-        
-      
-      <Text className="color-[#182230] font-bold text-[32px] mb-[24px]">
-        {userName}님 {'\n'}반가워요!
-      </Text>
-      <Text className="text-[20px] font-medium color-[#667085]">
-        당신 곁의 소리를 들어드릴게요!
-      </Text>
-    </Pressable>
+      <Header />
+      <Image className="w-full h-full absolute" source={bg} />
+      <Pressable
+        onPressIn={() => {
+          moveLoc('home');
+        }}
+        className="flex flex-1 justify-center items-center">
+        <Text className="color-[#182230] font-bold text-[32px]">
+          {userName}님
+        </Text>
+        <Text className="color-[#182230] font-bold text-[32px] mb-[24px]">
+          반가워요!
+        </Text>
+        <Text className="text-[20px] font-medium color-[#667085]">
+          당신 곁의 소리를 들어드릴게요!
+        </Text>
+      </Pressable>
     </>
   );
 }
