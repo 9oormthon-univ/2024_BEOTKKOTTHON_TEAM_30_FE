5dfb5c900cd0ecc4efca3a7b5cd988ce90f25ea5
9ye1 <jastu8646@gmail.com>
2024-03-19 19:39:46 +0900

    testcode-1

--- components/HapticTest.jsx
+++ components/HapticTest.jsx
@@ -1,38 +1,35 @@
-import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {Text} from 'react-native';
 import { HapticEngine } from 'react-native-core-haptics-api';
 
-export default async function HapticTest(){
+export default function HapticTest(){
   
-  const capabilities = await HapticEngine.getDeviceCapabilities();
-    if (!capabilities.supportsHaptics) {
-        throw new Error("Device does not support haptics");
+  const hapticStart = async () => {
+
+    const capabilities = await HapticEngine.getDeviceCapabilities();
+      if (!capabilities.supportsHaptics) {
+        throw new Error('Device does not support haptics');
+      }
+  
+    const hapticEvent = {
+      parameters: [{
+        parametersID: {
+          rawValue: "HapticIntencity"
+        },
+        value: 1,
+      },{
+        parametersID: {
+          rawValue: "HapticSharpness",
+        },
+        value : 0.1
+      }],
+      eventType: {
+        rawValue: "HapticContinuous"
+      },
+      duration: 0.5,
+      relativeTime: 0
     }
-
-
-  const hapticEvent = {
-    parameters: [{
-      parametersID: {
-        rawValue: "HapticIntencity"
-      },
-      value: 1,
-    },{
-      parametersID: {
-        rawValue: "HapticSharpness",
-      },
-      value : 0.1
-    }],
-    eventType: {
-      rawValue: "HapticContinuous"
-    },
-    duration: 0.5,
-    relativeTime: 0
-  }
   
- 
-  
-  // 버튼 눌렀을 때 햅틱 실행 함수
-  const onPressButton = async () => {
-    
+
     await HapticEngine.start(undefined);
     await HapticEngine.makePlayer(hapticEvent, undefined);
     let currentTime = 0.0;
@@ -50,16 +47,24 @@ export default async function HapticTest(){
 
   };
 
+  useEffect(() => {
+    hapticStart()
+      .then(() => {
+        setResult("Finished playing example! :)");
+      })
+      .catch(() => {
+        setResult("Unable to play example");
+      })
+  }, []);
   
   return (
-    <TouchableOpacity onPress={onPressButton} style={styles.button}>
-      <Text style={styles.buttonText}>Press me</Text>
-    </TouchableOpacity>
+    <View>
+      <Text>haptic</Text>
+    </View>
   );
 
 
-};
-
+/*
 const styles = StyleSheet.create({
   button: {
     padding: 20,
@@ -70,4 +75,6 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 18,
   },
-});
+});*/
+
+}
