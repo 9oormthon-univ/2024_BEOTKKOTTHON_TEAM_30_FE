244c3eae6bc493763cd4b8dd08ba6aadc48cfe4e
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-03-20 02:46:26 +0900

    toast message layout

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import {View} from 'react-native';
+import {Button, View, Text, Image} from 'react-native';
 import VoicRecog from './components/VoiceRecog';
 import Toast from 'react-native-toast-message';
 import {NavigationContainer} from '@react-navigation/native';
@@ -14,12 +14,30 @@ import {UserLocContextProvider} from './contexts/userloc';
 import Content from './components/Content';
 import {KeywordsContextProvider} from './contexts/keywords';
 
+
+import loverIcon from './assets/twinkleLover.png';
+import friendIcon from './assets/twinkleFriend.png';
+import workIcon from './assets/twinkleWork.png';
+import familyIcon from './assets/twinkleFamily.png';
+import dailyIcon from './assets/twinkleDaily.png';
+import emergencyIcon from './assets/twinkleRed.png';
+
+
+const toastConfig = {
+  newToast: ({text1}) => (
+    <View className="border-solid border-[1px] border-gray-300 rounded-xl flex flex-row justify-center items-center w-[345px] h-[56px] bg-white">
+      <Image source={loverIcon} />
+      <Text>{text1}</Text>
+    </View>
+  ),
+};
+
 function App(): React.JSX.Element {
   function showToast() {
     Toast.show({
-      type: 'success',
-      text1: 'keyword recognized!',
-      text2: 'keyword',
+      type: 'newToast',
+      text1: '친구가 구름님을 부르고 있나봐요!',
+      
     });
   }
 
@@ -31,11 +49,11 @@ function App(): React.JSX.Element {
             <Content />
             {/* <VoicRecog /> */}
 
-            {/* <Button title="show toast" onPress={showToast}>
-          click here to test Toast message
-        </Button> */}
+            <Button title="show toast" onPress={showToast}>
+              click here to test Toast message
+            </Button>
           </View>
-          <Toast />
+          <Toast config={toastConfig} />
         </NavigationContainer>
       </KeywordsContextProvider>
     </UserLocContextProvider>
